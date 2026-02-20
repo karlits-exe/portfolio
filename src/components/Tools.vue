ec5c23cb6242265e3b76070d96edeb0c76645f72
Karl Dennis <k.dennisph@gmail.com>
2026-02-20 13:31:35 +0800

    updated tools

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -1,77 +1,60 @@
+
 <template>
-    <div id="tools" class="my-5 py-5">
-      <div class="container">
-        <div class="row gy-5">
-          <h1 class="text-center">Tools</h1>
-          <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card">
-              <img
-                src="/images/html.png"
-                class="card-img-top img-fluid"
-                alt="..."
-              />
-              <div class="card-body">
-                <p class="card-text text-center">HTML</p>
-              </div>
+	<div class="row flex-column bg-white jumbotron" id="tools">
+        <h1 class="my-1 text-center" >Tools</h1>
+        <div class="row justify-content-center py-3 tools-img">
+            <div class="col-md-3 col-lg-2 mx-4 my-3">
+                <h3 class="my-3 text-center">Front End</h3>
+                <div class="row justify-content-center tools-img gx-5">
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/html.svg" class="img-fluid">
+                    </div>
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/css.svg" class="img-fluid">
+                    </div>
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/bootstrap.svg" class="img-fluid">
+                    </div>
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/js.svg" class="img-fluid">
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card">
-              <img src="/images/css.png" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <p class="card-text text-center">CSS</p>
-              </div>
+            <div class="col-md-3 col-lg-2 mx-4 my-3">
+                <h3 class="my-3 text-center">MERN</h3>
+                <div class="row tools-img gx-5">
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/mongodb.svg" class="img-fluid">
+                    </div>
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/expressjs.svg" class="img-fluid">
+                    </div>
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/react.svg" class="img-fluid">
+                    </div>
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/nodejs.svg" class="img-fluid">
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card">
-              <img
-                src="/images/bootstrap1.png"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <p class="card-text text-center">Bootstrap</p>
-              </div>
+            <div class="col-md-3 col-lg-2 mx-4 my-3">
+                <h3 class="my-3 text-center">Other Tools</h3>
+                <div class="row tools-img gx-5">
+
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/heroku.svg" class="img-fluid">
+                    </div>
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/git.svg" class="img-fluid">
+                    </div>
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/postman.svg" class="img-fluid">
+                    </div>
+                    <div class="col-6 tools-img">
+                        <img src="/images/tools/sublime.svg" class="img-fluid">
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card">
-              <img
-                src="/images/javascript.png"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <p class="card-text text-center">JavaScript</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card">
-              <img src="/images/git.png" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <p class="card-text text-center">Git</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card">
-              <img src="/images/github.png" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <p class="card-text text-center">Github</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
-            <div class="card">
-              <img src="/images/figma.png" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <p class="card-text text-center">Figma</p>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
 </template>
